7ed5777e2829180505a86e60c23c0775b97f016b
Banner2404 <zhekon2404@gmail.com>
2016-08-03 11:15:07 +0300

    Initial Commit

---
+++ SportTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SportTime
+//
+//  Created by Соболь Евгений on 03.08.16.
+//  Copyright © 2016 Соболь Евгений. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
